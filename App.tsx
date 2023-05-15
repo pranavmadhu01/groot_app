f64ef2ba4328e99643c03773cb69fe01646084a2
Noah <noahjoh11@gmail.com>
2023-05-15 22:21:50 +0530

    splash screen set

--- App.tsx
+++ App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import Addfarm from './src/screens/postlogin/Addfarm';
-import Farmform from './src/screens/postlogin/Farmform';
-import Landingpage from './src/screens/prelogin/Landingpage';
-import Login from './src/screens/prelogin/Login';
-import Opener from './src/screens/prelogin/Opener';
-import Signup from './src/screens/prelogin/Signup';
-import CostEstimatorForm from './src/screens/postlogin/CostEstimatorForm';
-import DiseaseInfo from './src/screens/postlogin/DiseaseInfo';
-import Home from './src/screens/postlogin/Home';
+import React, {useEffect} from 'react';
+
+import SplashScreen from 'react-native-splash-screen';
+
 import {NavigationContainer} from '@react-navigation/native';
 import BottomNav from './src/navigation/BottomNav';
 
 const App = () => {
+  useEffect(() => {
+    const hideSplashScreen = () => {
+      SplashScreen.hide();
+    };
+
+    const timeout = setTimeout(hideSplashScreen, 300);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <NavigationContainer>
       <BottomNav />
